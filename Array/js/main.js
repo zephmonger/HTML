8e1007609515a16a4abf22435b3bef5e6f8961d8
Ramin <56130083+zephmonger@users.noreply.github.com>
2019-11-12 12:47:56 -0800

    unfinished

---
+++ Array/js/main.js
@@ -0,0 +1,54 @@
+//alert("hello");
+// {let studentArray = ["Leonardo", "Raphael", "Donatello", "Michalangelo"];
+// 	console.log("The first student name is: " + studentArray[3]);
+// var cars = ["Saab", "Volvo", "BMW"];
+// document.getElementById("demo").innerHTML = cars[2];
+// }
+// {var fruits, text, fLen, i;
+// fruits = ["Banana", "Orange", "Apple", "Mango", "Beef"];
+// fLen = fruits.length;
+
+// text = "<ul>";
+// for (i = 0; i < fLen; i++) {
+//   text += "<li>" + fruits[i] + "</li>";
+// }
+// text += "</ul>";
+// document.getElementById("demo").innerHTML = text;}
+
+// function arrays(){
+// var numbers = [36, 52, 38, 27, 88];
+// var sum = 0
+// 	{document.getElementById("num").innerHTML = numbers;
+		
+// 	}
+// }
+// function check(){
+// 	//Declare
+// 	let num1 = parseInt(document.getElementById("number1").value);
+// 	let num2 = parseInt(document.getElementById("number2").value);
+// 	let num3 = parseInt(document.getElementById("number3").value);
+
+// 	//Process
+// 	let sumNum = parseInt(num1 + num2 + num3);
+// 	//or let average = parseInt(sumNum/3)
+// 	let averageNum = parseInt((num1 + num2 + num3)/3);
+
+// 	//Outcome. ---.checked makes sure radio button is checked
+// 	if (document.getElementById('sum').checked) {
+// 		document.getElementById('result').innerHTML = sumNum;
+// 	} else if(documnet
+
+
+
+// }
+
+function convert(){
+	let cm = parseInt(document.getElementById('cm1').value);
+	let sum= (cm/2.54);
+
+	document.getElementById("result").innerHTML = sum;
+}
+
+
+
+
